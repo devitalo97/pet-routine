5bef7dea139cd6f0c76314b4559c2a52970a9320
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-06-04 16:45:29 -0300

    feat(pack/back-end): add one more method on base repository interface

--- packages/back-end/src/core/domain/@shared/repository.interface.ts
+++ packages/back-end/src/core/domain/@shared/repository.interface.ts
@@ -4,6 +4,7 @@ interface IRepository<E extends Entity<EntityData>> {
 	insert(entity: E): Promise<void>;
 	update(entity: E): Promise<void>;
 	delete(id: string): Promise<void>;
+	findById(id: string): Promise<E | null>;
 }
 
 export type { IRepository };
