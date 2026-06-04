14afb2b4fe222f61161b8bf90be031d8c8df5327
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-06-04 16:30:31 -0300

    feat(pack/back-end): declare the pet schema

---
+++ packages/back-end/src/infrastructure/db/schema/pet.schema.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const petTable = pgTable("pet", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar({ length: 255 }).notNull(),
+	breed: varchar({ length: 255 }).notNull(),
+	specie: varchar({ length: 255 }).notNull(),
+	sex: varchar({ length: 255 }).notNull(),
+	birthday: timestamp({ withTimezone: true }).notNull(),
+	createdAt: timestamp("created_at", { withTimezone: true }).notNull(),
+	updatedAt: timestamp("updated_at", { withTimezone: true }).notNull(),
+});
