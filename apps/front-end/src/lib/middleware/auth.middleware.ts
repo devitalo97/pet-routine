8dee51dc692bb68bf7a2f55905db69284ed66529
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-06-04 21:05:52 -0300

    feat(apps/front-end): create an auth middleware to use on server functions

---
+++ apps/front-end/src/lib/middleware/auth.middleware.ts
@@ -0,0 +1,18 @@
+// middleware.ts
+
+import { createMiddleware } from "@tanstack/react-start";
+import { auth } from "@/lib/auth";
+
+export const authMiddleware = createMiddleware().server(
+	async ({ next, request }) => {
+		const session = await auth.api.getSession({ headers: request.headers });
+
+		if (!session) {
+			throw new Error("Unauthorized");
+		}
+
+		return await next({
+			context: { session },
+		});
+	},
+);
