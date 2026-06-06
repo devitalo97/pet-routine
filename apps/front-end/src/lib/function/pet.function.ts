be4aaf6a7be2492fafe25ef62940737457e3f068
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-06-06 18:10:07 -0300

    feat(apps/front-end): create a new function to handle the find one pet use case

--- apps/front-end/src/lib/function/pet.function.ts
+++ apps/front-end/src/lib/function/pet.function.ts
@@ -17,3 +17,14 @@ export const createPetFn = createServerFn({ method: "POST" })
 	.handler(async ({ data }) => {
 		return backend.petCreationUseCase.execute(data);
 	});
+
+export const findOnePetFn = createServerFn({ method: "GET" })
+	.inputValidator(
+		z.object({
+			id: z.string(),
+		}),
+	)
+	.middleware([authMiddleware])
+	.handler(async ({ data }) => {
+		return backend.petFindOneUseCase.execute(data);
+	});
