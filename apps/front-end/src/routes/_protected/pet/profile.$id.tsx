79575fdf03fbff2cb92458ab927b391106527fb8
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-06-07 11:50:09 -0300

    feat(apps/front-end): use the error page component in pet profile route

--- apps/front-end/src/routes/_protected/pet/profile.$id.tsx
+++ apps/front-end/src/routes/_protected/pet/profile.$id.tsx
@@ -1,5 +1,6 @@
-import { createFileRoute, notFound } from "@tanstack/react-router";
+import { createFileRoute, isNotFound, notFound } from "@tanstack/react-router";
 import { NotFoundEntityPage } from "#/components/page/entity.not-found.page";
+import { ErrorPage } from "#/components/page/error.page";
 import { PetProfilePage } from "#/components/page/pet.profile.page";
 import { findOnePetFn } from "#/lib/function/pet.function";
 
@@ -7,14 +8,18 @@ export const Route = createFileRoute("/_protected/pet/profile/$id")({
 	notFoundComponent: () => (
 		<NotFoundEntityPage title="Não foi possível encontrar o pet" />
 	),
+	errorComponent: ({ error }) => <ErrorPage title={error.message} />,
 	component: PetProfilePage,
 	loader: async ({ params }) => {
 		try {
 			const pet = await findOnePetFn({ data: { id: params.id.toString() } });
 			if (!pet) throw notFound();
 			return pet;
-		} catch {
-			throw notFound();
+		} catch (error) {
+			if (isNotFound(error)) {
+				throw error;
+			}
+			throw Error("Erro ao buscar o pet");
 		}
 	},
 });
